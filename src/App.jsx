594509a5599efa36afdd969c0c58518edc0e636e
vineet210803 <vineet210803@gmail.com>
2025-09-30 00:04:42 +0530

    name change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import Login from "./components/Auth/Login";
-import EmployeeDashboard from "./components/Dashboard/employeeDashboard";
-import AdminDashboard from "./components/Dashboard/adminDashboard";
+import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
+import AdminDashboard from "./components/Dashboard/AdminDashboard";
 import { AuthContext } from "./context/AuthProvider";
 import { getLocalStorage, setLocalStorage } from "./utils/localStorage";
 
